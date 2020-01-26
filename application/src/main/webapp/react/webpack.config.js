06e4794a95ad90e7c54dd71e8591a394c5cb9134
ShaneCreedon <shane.creedon3@mail.dcu.ie>
2020-01-26 18:51:40 +0000

    - React Structure In-place. - React Dev server ready.
    Signed-off-by: ShaneCreedon <shane.creedon@brightflag.com>

--- application/src/main/webapp/react/webpack.config.js
+++ application/src/main/webapp/react/webpack.config.js
@@ -1,5 +1,22 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+// NOTE: webpack watch options has a poll setting, do not include this if you are using webpack-dev-server
+// Polling dramatically influences the cpu usage.
+
 module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: 'bundle.js'
+  },
+  devServer: {
+    public: 'http://localhost:3000/dist',
+    publicPath: 'http://localhost:3000/dist',
+    host: "localhost",
+    port: 3000,
+    hot: true,
+    watchContentBase: true,
+    disableHostCheck: true,
+  },
+  devtool: 'source-map',
+  mode: "development",
   module: {
     rules: [
       {
@@ -8,21 +25,7 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
       }
     ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.jsp",
-      filename: "./index.jsp"
-    })
-  ]
+  }
 };

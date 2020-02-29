87365ee91d2431773295cab15a91ee17a7e2329f
Shane Creedon <shane.creedon3@mail.dcu.ie>
2020-02-29 15:45:43 +0000

    - Enable login via the enter key
    Signed-off-by: ShaneCreedon <shane.creedon@brightflag.com>

--- application/src/main/webapp/react/src/components/molecules/form/LoginForm/index.tsx
+++ application/src/main/webapp/react/src/components/molecules/form/LoginForm/index.tsx
@@ -9,10 +9,18 @@ const LoginForm = (props: any) => {
     const [username, setUsername] = useState(null);
     const [password, setPassword] = useState(null);
 
+    const keyPress = (event: React.KeyboardEvent) => {
+        if (event.key == "Enter") {
+            props.checkCredentials(username, password);
+        }
+    };
+
     return (
         <div className={"login-form"}>
-            <TextInputField title={"Email: "} type={"email"} update={(value: string) => setUsername(value)}/>
-            <TextInputField title={"Password: "} type={"password"} update={(value: string) => setPassword(value)}/>
+            <TextInputField title={"Email: "} type={"email"} update={(value: string) => setUsername(value)}
+                            keypress={(e: React.KeyboardEvent) => keyPress(e)}/>
+            <TextInputField title={"Password: "} type={"password"} update={(value: string) => setPassword(value)}
+                            keypress={(e: React.KeyboardEvent) => keyPress(e)}/>
             <PrimaryButton text={props.text} click={() => props.checkCredentials(username, password)}/>
         </div>
     );

ac2cc735de49fa48b2121a194fcfefba58d13d94
Shane Creedon <shane.creedon3@mail.dcu.ie>
2020-02-29 21:31:25 +0000

    Implement Flyway
    - Add flyway for DB maintenance and testing
    
    Signed-off-by: ShaneCreedon <shane.creedon@brightflag.com>

---
+++ application/src/main/resources/db/migration/V1_0_1__Initialize_database_tables_for_test.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `user` (
+  `userID` int(15) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(60) DEFAULT NULL,
+  `role` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `level` (
+  `levelID` int(15) NOT NULL AUTO_INCREMENT,
+  `level` int(60) DEFAULT NULL,
+  `title` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`levelID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `userlevel` (
+  `userlevelID` int(15) NOT NULL AUTO_INCREMENT,
+  `userID` int(15) DEFAULT NULL,
+  `levelID` int(15) DEFAULT NULL,
+  PRIMARY KEY (`userlevelID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;

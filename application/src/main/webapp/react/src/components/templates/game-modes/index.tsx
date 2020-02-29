7481d7725a5d72404273d4fd45af4a800282b5ec
Shane Creedon <shane.creedon3@mail.dcu.ie>
2020-02-29 18:02:28 +0000

    Add font-awesome
    Signed-off-by: ShaneCreedon <shane.creedon@brightflag.com>

--- application/src/main/webapp/react/src/components/templates/game-modes/index.tsx
+++ application/src/main/webapp/react/src/components/templates/game-modes/index.tsx
@@ -1,14 +1,33 @@
 import * as React from "react";
 import PrimaryButton from "../../atoms/button/primary";
 import "./style/style.css"
+import {faChessKing, faUser} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const GameModes = () => {
     return (
-        <div className={"button-form-container"}>
-            <PrimaryButton text={"Battle"} click={() => console.log("Battle Clicked")}/>
-            <PrimaryButton text={"Time Attack"} click={() => console.log("Time Attack Clicked")}/>
-            <PrimaryButton text={"War"} click={() => console.log("War Clicked")}/>
-            <PrimaryButton text={"Tag Team"} click={() => console.log("Tag Team Clicked")}/>
+        <div className={"game-modes wrapper"}>
+
+            <div className={"game-modes top-row"}>
+                <div className={"game-modes user-details"}>
+                    <FontAwesomeIcon icon={faChessKing} />
+                    <p> Level: 52 </p>
+                </div>
+                <div className={"game-modes title"}>
+                    <h2> Game Modes </h2>
+                </div>
+                <div className={"game-modes profile"}>
+                    <FontAwesomeIcon icon={faUser} />
+                    <i className="fas fa-user"> UserIDX </i>
+                </div>
+            </div>
+
+            <div className={"button-form-container"}>
+                <PrimaryButton text={"Battle"} click={() => console.log("Battle Clicked")}/>
+                <PrimaryButton text={"Time Attack"} click={() => console.log("Time Attack Clicked")}/>
+                <PrimaryButton text={"War"} click={() => console.log("War Clicked")}/>
+                <PrimaryButton text={"Tag Team"} click={() => console.log("Tag Team Clicked")}/>
+            </div>
         </div>
     );
 };

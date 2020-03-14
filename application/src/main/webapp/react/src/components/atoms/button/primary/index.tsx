6791a6231365258b307f8a556adf657f27c3c6f3
Shane Creedon <shane.creedon3@mail.dcu.ie>
2020-03-14 18:13:36 +0000

    - Fix css issue where login form component title would slide off screen, required wrapper div. - Alter PrimaryButton component to be generic BootstrapButton component such that we can vary colour.
    Signed-off-by: ShaneCreedon <shane.creedon@brightflag.com>

--- application/src/main/webapp/react/src/components/atoms/button/primary/index.tsx
+++ application/src/main/webapp/react/src/components/atoms/button/primary/index.tsx
@@ -1,15 +1,17 @@
 import * as React from "react";
 import "./style/style.css";
 
-export interface PrimaryButtonType {
+export interface BootstrapButtonType {
     className?: string,
     click: Function,
     enabled?: boolean,
     text: string,
+    bootstrapColor?: string
 }
 
 const defaultProps: Object = {
-    enabled: true
+    enabled: true,
+    bootstrapColor: "primary"
 };
 
 /**
@@ -18,19 +20,20 @@ const defaultProps: Object = {
  * - click: Function -- callback method when button clicked
  * - enabled: boolean -- Whether button enabled or disabled
  * - text: string -- Text of the button
+ * - bootstrap-color: string -- Examples: {primary, secondary, success, danger, warning, info, light, dark, link}
  * @param props
  * @constructor
  */
-const PrimaryButton: React.FunctionComponent<PrimaryButtonType> = (props:PrimaryButtonType) => {
+const BootstrapButton: React.FunctionComponent<BootstrapButtonType> = (props:BootstrapButtonType) => {
     return (
       <div className={props.className}>
-          <button type="button" className="btn btn-primary custom" onClick={(event: React.MouseEvent<HTMLElement>) => {
+          <button type="button" className={`btn btn-${props.bootstrapColor} custom`} onClick={(event: React.MouseEvent<HTMLElement>) => {
               props.click(event);
           }} disabled={!props.enabled}> {props.text} </button>
       </div>
     );
 };
 
-PrimaryButton.defaultProps = defaultProps;
+BootstrapButton.defaultProps = defaultProps;
 
-export default PrimaryButton;
+export default BootstrapButton;

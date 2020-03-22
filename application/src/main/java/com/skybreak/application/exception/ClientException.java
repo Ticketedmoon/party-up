b3c7e877f2877ea4d18b6dc5b8bed469aa13ffb4
Shane Creedon <shane.creedon3@mail.dcu.ie>
2020-03-22 21:35:51 +0000

    - Add subErrorList to clientException.java
    Signed-off-by: ShaneCreedon <shane.creedon@brightflag.com>

--- application/src/main/java/com/skybreak/application/exception/ClientException.java
+++ application/src/main/java/com/skybreak/application/exception/ClientException.java
@@ -1,6 +1,7 @@
 package com.skybreak.application.exception;
 
 import java.time.ZonedDateTime;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 
 public class ClientException {
@@ -8,11 +9,19 @@ public class ClientException {
     private String message;
     private HttpStatus httpStatus;
     private ZonedDateTime zonedDateTime;
+    private List<String> subErrorList;
 
     public ClientException(String message, HttpStatus httpStatus, ZonedDateTime zonedDateTime) {
         this.message = message;
         this.httpStatus = httpStatus;
         this.zonedDateTime = zonedDateTime;
+    }
+
+    public ClientException(String message, HttpStatus httpStatus, ZonedDateTime zonedDateTime, List<String> subErrorList) {
+        this.message = message;
+        this.httpStatus = httpStatus;
+        this.zonedDateTime = zonedDateTime;
+        this.subErrorList = subErrorList;
     }
 
     public String getMessage() {
@@ -37,5 +46,13 @@ public class ClientException {
 
     public void setZonedDateTime(ZonedDateTime zonedDateTime) {
         this.zonedDateTime = zonedDateTime;
+    }
+
+    public List<String> getSubErrorList() {
+        return subErrorList;
+    }
+
+    public void setSubErrorList(List<String> subErrorList) {
+        this.subErrorList = subErrorList;
     }
 }

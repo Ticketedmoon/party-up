8cde1b1ba1f4e2d0df37ea9f39362e465912bf86
Shane Creedon <shane.creedon3@mail.dcu.ie>
2020-04-25 14:42:58 +0100

    - Upgrade Junit - Upgrade Mockito - Add first unit test
    Signed-off-by: ShaneCreedon <shane.creedon@brightflag.com>

--- application/src/main/java/com/skybreak/application/service/UserService.java
+++ application/src/main/java/com/skybreak/application/service/UserService.java
@@ -9,17 +9,19 @@ import com.skybreak.application.exception.UsernameNotValidException;
 import com.skybreak.application.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-    private static Logger logger = LoggerFactory.getLogger(LoginController.class);
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
-    private PasswordEncoder passwordEncoder;
-    private UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
+    @Autowired
     public UserService(PasswordEncoder passwordEncoder, UserRepository userRepository) {
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;

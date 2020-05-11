fc1b7f04b20815ad121968371e55e56e290d0769
Shane Creedon <shane.creedon3@mail.dcu.ie>
2020-05-11 13:23:26 +0100

    feature/integrate-web-sockets-with-chat-feature - Websocket response to subscribe event now working.
    Signed-off-by: ShaneCreedon <shane.creedon@brightflag.com>

--- application/src/main/java/com/codewars/application/configuration/SocketBrokerConfiguration.java
+++ application/src/main/java/com/codewars/application/configuration/SocketBrokerConfiguration.java
@@ -12,27 +12,8 @@ public class SocketBrokerConfiguration implements WebSocketMessageBrokerConfigur
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        /*
-         * The application destination prefix is an arbitrary prefix to
-         * differentiate between messages that need to be routed to
-         * message-handling methods for application level work vs messages to be
-         * routed to the broker to broadcast to subscribed clients. After
-         * application level work is finished the message can be routed to
-         * broker for broadcasting.
-         */
         registry.setApplicationDestinationPrefixes("/app");
-
-
-        /*
-         * The list of destination prefixes provided in this are based on what
-         * broker is getting used. In this case we will use in-memory broker
-         * which doesn't have any such requirements. For the purpose of
-         * maintaining convention the "/topic" and the "/queue" prefixes are
-         * chosen. The convention dictates usage of "/topic" destination for
-         * pub-sub model targeting many subscribers and the "/queue" destination
-         * for point to point messaging.
-         */
-        registry.enableSimpleBroker("/chat/topic");
+        registry.enableSimpleBroker("/topic");
     }
 
     @Override

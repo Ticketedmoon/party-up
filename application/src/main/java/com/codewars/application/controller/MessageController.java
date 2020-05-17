e6a159a4ad55a10bdf78224456da1ebdf9a48086
Shane Creedon <shane.creedon3@mail.dcu.ie>
2020-05-18 00:16:54 +0100

    feature/integrate-web-sockets-with-chat-feature: - Change architecture slightly of front-end components - Change architecture slightly of backend newUser logic and create DTOs for different purposes. - Add an 'Online users' container to show the online users alongside the chat log.
    Signed-off-by: ShaneCreedon <shane.creedon@brightflag.com>

--- application/src/main/java/com/codewars/application/controller/MessageController.java
+++ application/src/main/java/com/codewars/application/controller/MessageController.java
@@ -1,7 +1,9 @@
 package com.codewars.application.controller;
 
+import com.codewars.application.domain.dto.UserConnectedDTO;
 import com.codewars.application.domain.entity.ChatMessage;
 import com.codewars.application.service.MessageService;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -32,11 +34,19 @@ public class MessageController {
     }
 
     @MessageMapping("/chat.newUser")
-    @SendTo("/topic/chat")
-    public ChatMessage newUserJoined(@Payload final ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+    @SendTo("/topic/chat/newUser")
+    public UserConnectedDTO newUserJoined(@Payload final ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
-        chatMessage.setContent(String.format("User %s has joined the session.", chatMessage.getSender()));
-        return chatMessage;
+        chatMessage.setContent(String.format("User %s has joined the session.", chatMessage.getSender().getUsername()));
+        chatMessage.getSender().setUserId(UUID.randomUUID().toString());
+
+        // TOOD: Maybe consider moving this logic to some utility service, OR find a way to automatically parse it on request.
+        messageService.addNewConnectedUsers(chatMessage.getSender());
+
+        UserConnectedDTO userConnectedDTO = new UserConnectedDTO();
+        userConnectedDTO.setConnectedUsers(messageService.getConnectedUsers());
+        userConnectedDTO.setChatMessage(chatMessage);
+        return userConnectedDTO;
     }
 
 }
--- application/src/main/webapp/react/src/components/templates/template.public.chat.tsx
+++ application/src/main/webapp/react/src/components/templates/template.public.chat.tsx
@@ -1,8 +1,84 @@
-import React, {FunctionComponent} from "react";
+import React, {FunctionComponent, useEffect, useRef, useState} from "react";
 import {ChatView} from "../organisms/chat-view";
+import {OnlineUserView} from "../organisms/online-user-view";
+import {IMessageTypeResponse} from "../atoms/message/chat.message.enum";
+import {Client} from "@stomp/stompjs";
+import {IChatMessageResponse} from "../atoms/message/chat.message.interface";
+import {RootStateOrAny, useSelector} from "react-redux";
+
+const style = require("./style/style.module.css");
 
 export const PublicChatTemplate: FunctionComponent<any> = () => {
+
+    const MESSAGE_LOG_LIMIT = 50;
+
+    const user = useSelector((state: RootStateOrAny) => state.activeUser);
+    const client = useRef(new Client());
+    const [connectedUsers, setConnectedUsers] = useState({});
+    const [messageLog, setMessageLog] = useState<IChatMessageResponse[]>([]);
+
+    const addMessage: Function = (message: IChatMessageResponse) => {
+        setMessageLog((state: IChatMessageResponse[]) => state.concat(message));
+    };
+
+    const disconnectClient = () => {
+        client.current.deactivate();
+    }
+
+    const moveScrollBarToShowNewMessage = () => {
+        let chatRegion = document.querySelector('#chat-region');
+        if (chatRegion) {
+            chatRegion.scrollTop = chatRegion.scrollHeight - chatRegion.clientHeight;
+        }
+    }
+
+    useEffect(() => {
+        client.current.configure({
+            // TODO: Update this to be base path rather than hard-coded "localhost"
+            brokerURL: 'ws://localhost:8080/chat-app',
+            onConnect: () => {
+                client.current.subscribe('/topic/chat', (message) => {
+                    addMessage(JSON.parse(message.body));
+                    moveScrollBarToShowNewMessage();
+                });
+
+                client.current.subscribe('/topic/chat/newUser', (message) => {
+                    let response = JSON.parse(message.body);
+                    addMessage(response.chatMessage);
+                    setConnectedUsers(response.connectedUsers);
+                    moveScrollBarToShowNewMessage();
+                });
+
+                client.current.publish({
+                    destination: '/app/chat.newUser', body:
+                        JSON.stringify({
+                            'type': IMessageTypeResponse.CHAT,
+                            'sender': {"username": user.username},
+                            'time': new Date()
+                        })
+                })
+            },
+            // Helps during debugging, remove in production
+            debug: (str) => {
+                console.log(new Date(), str);
+            }
+        })
+
+        client.current.activate();
+    }, [])
+
+    useEffect(() => {
+        if (messageLog.length > MESSAGE_LOG_LIMIT) {
+            let messageLogWithoutFirst = [...messageLog];
+            messageLogWithoutFirst.splice(0, 1);
+            setMessageLog(messageLogWithoutFirst);
+        }
+    }, [messageLog])
+
     return (
-        <ChatView/>
+        <div className={style["position-container"]}>
+            <ChatView client={client} disconnectClient={disconnectClient} messageLog={messageLog}/>
+            <OnlineUserView userList={Object.values(connectedUsers)}/>
+        </div>
     )
 }

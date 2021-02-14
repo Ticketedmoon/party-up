6536a3f5d5fa12f12fa058b0a54e18f33a2f0043
Shane Creedon <shane.creedon3@mail.dcu.ie>
2021-02-14 01:07:48 +0000

    MASTER:
    Signed-off-by: ShaneCreedon <shane.creedon@brightflag.com>

--- application/src/main/java/com/partyup/application/configuration/context/ApplicationPersistenceConfiguration.java
+++ application/src/main/java/com/partyup/application/configuration/context/ApplicationPersistenceConfiguration.java
@@ -2,14 +2,50 @@ package com.partyup.application.configuration.context;
 
 import java.util.Properties;
 import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 @Configuration
+@PropertySource("classpath:application.properties")
+@Slf4j
 public class ApplicationPersistenceConfiguration {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        propertySourcesPlaceholderConfigurer.setLocations(new ClassPathResource("application.properties"));
+        return propertySourcesPlaceholderConfigurer;
+    }
+
+    @Profile("development")
+    @Bean(name = "dataSource")
+    public DataSource createDevelopmentDataSource(@Value("${dev.app.datasource.driver}") String driverClassName,
+                                                  @Value("${dev.app.datasource.url}") String dataSourceUrl,
+                                                  @Value("${dev.app.datasource.username}") String dataSourceUsername,
+                                                  @Value("${dev.app.datasource.password}") String dataSourcePassword) {
+        log.info("Data Source Creation for environment profile: [development]");
+        return buildDataSource(driverClassName, dataSourceUrl, dataSourceUsername, dataSourcePassword);
+    }
+
+    @Profile("production")
+    @Bean(name = "dataSource")
+    public DataSource createProductionDataSource(@Value("${prod.app.datasource.driverClassName}") String driverClassName,
+                                                 @Value("${prod.app.datasource.url}") String dataSourceUrl,
+                                                 @Value("${prod.app.datasource.username}") String dataSourceUsername,
+                                                 @Value("${prod.app.datasource.password}") String dataSourcePassword) {
+        log.info("Data Source Creation for environment profile: [production]");
+        return buildDataSource(driverClassName, dataSourceUrl, dataSourceUsername, dataSourcePassword);
+    }
 
     @Bean(name = "entityManager")
     public LocalContainerEntityManagerFactoryBean createLocalContainerEntityManagerFactoryBean(DataSource dataSource) {
@@ -36,6 +72,15 @@ public class ApplicationPersistenceConfiguration {
         JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
         jpaTransactionManager.setDataSource(dataSource);
         return jpaTransactionManager;
+    }
+
+    private DataSource buildDataSource(String driverClassName, String dataSourceUrl, String dataSourceUsername, String dataSourcePassword) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(dataSourceUrl);
+        dataSource.setUsername(dataSourceUsername);
+        dataSource.setPassword(dataSourcePassword);
+        return dataSource;
     }
 
 }
